9e2d19dcc4b24d06df7cd8b7d8924f26211757d9
aavidsoft-developer <aavidsoft.developer@gmail.com>
2023-07-29 12:54:08 +0530

    Inheritance part 1.1

--- src/Derived.java
+++ src/Derived.java
@@ -1,6 +1,19 @@
 public class Derived extends Base {
     private int d;
 
+    public Derived() {
+        System.out.println("Derived()");
+        d = 20;
+    }
+
+    public Derived(int b, int d) {
+        super(b);
+        System.out.println("Derived(int, int)");
+        //this.b = b; //can not be done
+        //setB(b); not needed now
+        this.d = d;
+    }
+
     public int getD() {
         return d;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
+        Derived d1 = new Derived();
+        d1.display();
+        System.out.println();
+
+        Derived d2 = new Derived(100, 200);
+        d2.display();
+
+
         /*Base b1 = new Base();
         b1.setB(99);*/
 
-        Derived d1 = new Derived();
+        /*Derived d1 = new Derived();
         d1.setB(100);
         d1.setD(200);
         System.out.println(d1.getB() + " " + d1.getD());
         d1.display();
         d1.displayD();
-        d1.display();
+        d1.display();*/
 
         /*System.out.println();
 
